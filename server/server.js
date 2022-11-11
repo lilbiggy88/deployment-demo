5e74a8c952f8da4b6e032e5897d1b69481fac787
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-11-10 22:21:01 -0700

    commiting middleware

--- server/server.js
+++ server/server.js
@@ -6,14 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req,res) {
-    // res.sendFile(path.join(__dirname, "../index.html")) before getting it to heroku
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-});
+//Middleware
+app.use(express.static(path.join(__dirname, "../public"));)
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-});
+//Endpoints
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public"));
+// })
+
+// app.get("/", function(req,res) {
+//     // res.sendFile(path.join(__dirname, "../index.html")) before getting it to heroku
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// });
 
 const port = process.env.PORT || 4005;
 
