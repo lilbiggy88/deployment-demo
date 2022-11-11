e4a459c4dcff3ddffc6d3f3e142c6e6092a1df95
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-11-10 22:09:19 -0700

    third commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req,res) {
-    res.sendFile(path.join(__dirname, "../index.html"))
+    // res.sendFile(path.join(__dirname, "../index.html")) before getting it to heroku
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 4005;
